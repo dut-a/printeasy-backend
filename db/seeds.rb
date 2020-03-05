8cc2a52ba2ee506f6166db1c66618c261c8b3da5
dut-a <d.dut@live.com>
2020-03-05 12:20:29 -0600

    Updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -106,10 +106,19 @@ puts "End: Adding Addresses"
 puts "Begin: Adding Services (merchants only)"
 merchant_ids = User.where(user_type: "merchant").pluck(:id)
 periods = ["2 hours", "24 hours", `${3 * 24} hours`, "0.5 hour"]
-types = ["Photo", "Legal Document", "General Document", "3D"]
+print_types = [
+  "Offset Lithography",
+  "Flexography",
+  "Digital Printing",
+  "Large Format",
+  "Screen Printing",
+  "3D Printing",
+  "LED UV",
+  "Photo"
+]
 merchant_ids.length.times do
   Service.create(
-    service_type: types.sample,
+    service_type: print_types.sample,
     cost: Faker::Number.decimal(l_digits: 2, r_digits: 2),
     time_to_complete: periods.sample,
     user_id: merchant_ids.shift
@@ -124,14 +133,23 @@ customer_ids = User.where(user_type: "customer").pluck(:id)
 merchant_ids = User.where(user_type: "merchant").pluck(:id)
 service_ids = Service.pluck(:id)
 periods = ["2 hours", "1 day", "3 days", "30 minutes"]
-types = ["Photo", "Legal Document", "General Document", "3D"]
+print_types = [
+  "Offset Lithography",
+  "Flexography",
+  "Digital Printing",
+  "Large Format",
+  "Screen Printing",
+  "3D Printing",
+  "LED UV",
+  "Photo"
+]
 statuses = ["unstarted", "in progress", "completed"]
 
 customer_ids.length.times do
   current_customer_id = customer_ids.shift
   current_merchant_id = merchant_ids.shift
   Print.create(
-    print_type: types.sample,
+    print_type: print_types.sample,
     number_of_copies: [1, 3, 5, 9, 23, 8].sample,
     ordered_by: current_customer_id,
     fulfilled_by: current_merchant_id,
